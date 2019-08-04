8a671cf5bd71c7ec8870e1abc5702146fe70a939
Jake Ginnivan <jake.ginnivan@wanews.com.au>
2019-08-04 10:26:16 +0800

    Fixed up build

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { componentFactory, ComponentRegistrar } from './ComponentRegistrar'
+import { RouteBuilder } from './RouteBuilder'
+import { CompositionRegistrar } from './CompositionRegistrar'
+
+export { componentFactory, RouteBuilder, ComponentRegistrar, CompositionRegistrar }
