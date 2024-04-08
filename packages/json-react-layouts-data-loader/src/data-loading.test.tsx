fbbabd0328bc649765204a69972a0d9b8a3aec30
Nathan Phillis <nathan.phillis@wanews.com.au>
2024-04-08 17:31:24 +0800

    Update tests.

--- packages/json-react-layouts-data-loader/src/data-loading.test.tsx
+++ packages/json-react-layouts-data-loader/src/data-loading.test.tsx
@@ -11,7 +11,7 @@ configure({ adapter: new Adapter() })
 
 it('can load data for component', async () => {
     const resources = new DataLoaderResources<{}>()
-    const { middleware, createRegisterableComponentWithData } = init<{}>(resources)
+    const { getMiddleware, createRegisterableComponentWithData } = init<{}>(resources)
 
     const testComponentWithDataRegistration = createRegisterableComponentWithData(
         'test-with-data',
@@ -31,7 +31,7 @@ it('can load data for component', async () => {
         .registerComponents(registrar =>
             registrar
                 .registerComponent(testComponentWithDataRegistration)
-                .registerMiddleware(middleware),
+                .registerMiddleware(getMiddleware('component')),
         )
         .registerCompositions(registrar =>
             registrar.registerComposition(testCompositionRegistration),
@@ -67,12 +67,69 @@ it('can load data for component', async () => {
     expect(component.props()).toMatchSnapshot()
 })
 
+it('can load data for composition', async () => {
+    const resources = new DataLoaderResources<{}>()
+    const { getMiddleware, createRegisterableCompositionWithData } = init<{}>(resources)
+
+    const testCompositionWithDataRegistration = createRegisterableCompositionWithData<'main'>()(
+        'test-with-data',
+        lengthCalculatorDataDefinition,
+        ({ main }, props, data) => {
+            return (
+                <TestCompositionWithData
+                    main={main}
+                    length={data.loaded ? data.result : undefined}
+                    {...props}
+                    {...{ dataProps: { data } }}
+                />
+            )
+        },
+    )
+
+    const layout = LayoutRegistration()
+        .registerComponents(registrar => registrar.registerComponent(testComponentRegistration))
+        .registerCompositions(registrar =>
+            registrar
+                .registerComposition(testCompositionWithDataRegistration)
+                .registerMiddleware(getMiddleware('composition')),
+        )
+    const renderers = layout.createRenderers({
+        services: {},
+    })
+
+    const wrapper = mount(
+        <DataProvider resources={resources} globalProps={{}}>
+            {renderers.renderCompositions(
+                layout.composition({
+                    type: 'test-with-data',
+                    contentAreas: {
+                        main: [
+                            layout.component({
+                                type: 'test-component',
+                                props: {},
+                            }),
+                        ],
+                    },
+                    props: { dataDefinitionArgs: { dataArg: 'Foo' } },
+                }),
+            )}
+        </DataProvider>,
+    )
+
+    expect(wrapper.find(TestCompositionWithData).text()).toBe('Loading#TestComponent#')
+    await act(() => new Promise(resolve => setTimeout(resolve)))
+
+    const composition = wrapper.update().find(TestCompositionWithData)
+    expect(composition.text()).toBe('Length: 3#TestComponent#')
+    expect(composition.props()).toMatchSnapshot()
+})
+
 it('cap wrap data load function', async () => {
     let wrapArgs: any
     let wrapServices: any
     let wrapContext: any
     const resources = new DataLoaderResources<{ serviceProp: 'example' }>()
-    const { middleware, createRegisterableComponentWithData } = init<{ serviceProp: 'example' }>(
+    const { getMiddleware, createRegisterableComponentWithData } = init<{ serviceProp: 'example' }>(
         resources,
         load => (args, services, context) => {
             wrapArgs = args
@@ -100,7 +157,7 @@ it('cap wrap data load function', async () => {
         .registerComponents(registrar =>
             registrar
                 .registerComponent(testComponentWithDataRegistration)
-                .registerMiddleware(middleware),
+                .registerMiddleware(getMiddleware('component')),
         )
         .registerCompositions(registrar =>
             registrar.registerComposition(testCompositionRegistration),
@@ -139,7 +196,7 @@ it('cap wrap data load function', async () => {
 
 it('component can provide additional arguments dynamically', async () => {
     const resources = new DataLoaderResources<{}>()
-    const { middleware, createRegisterableComponentWithData } = init<{}>(resources)
+    const { getMiddleware, createRegisterableComponentWithData } = init<{}>(resources)
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     let updateMultiplier: (multiplier: number) => void = () => {}
 
@@ -187,7 +244,7 @@ it('component can provide additional arguments dynamically', async () => {
         .registerComponents(registrar =>
             registrar
                 .registerComponent(testComponentWithDataRegistration)
-                .registerMiddleware(middleware),
+                .registerMiddleware(getMiddleware('component')),
         )
         .registerCompositions(registrar =>
             registrar.registerComposition(testCompositionRegistration),
@@ -244,9 +301,118 @@ it('component can provide additional arguments dynamically', async () => {
     })
 })
 
-const { createRegisterableComposition } = getRegistrationCreators<{}>()
+it('composition can provide additional arguments dynamically', async () => {
+    const resources = new DataLoaderResources<{}>()
+    const { getMiddleware, createRegisterableCompositionWithData } = init<{}>(resources)
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    let updateMultiplier: (multiplier: number) => void = () => {}
 
-// Test component with data
+    const lengthCalculatorWithMultiplierDataDefinition: DataDefinition<
+        { dataArg: string },
+        number,
+        {},
+        { multiplier: number }
+    > = {
+        // Additional params can come from anywhere, for instance redux or
+        // other environmental variables (window.location?)
+        useRuntimeParams: () => {
+            const [multiplier, setMultiplier] = React.useState(2)
+            React.useEffect(() => {
+                updateMultiplier = setMultiplier
+            }, [])
+
+            return {
+                multiplier,
+            }
+        },
+        loadData: props =>
+            new Promise(resolve =>
+                setTimeout(() => {
+                    resolve(props.dataArg.length * props.multiplier)
+                }),
+            ),
+    }
+
+    const testCompositionWithDataRegistration = createRegisterableCompositionWithData<'main'>()(
+        'test-with-data',
+        lengthCalculatorWithMultiplierDataDefinition,
+        ({ main }, props, data) => {
+            return (
+                <TestCompositionWithData
+                    main={main}
+                    length={data.loaded ? data.result : undefined}
+                    {...props}
+                    {...{ dataProps: { data } }}
+                />
+            )
+        },
+    )
+
+    const layout = LayoutRegistration()
+        .registerComponents(registrar => registrar.registerComponent(testComponentRegistration))
+        .registerCompositions(registrar =>
+            registrar
+                .registerComposition(testCompositionWithDataRegistration)
+                .registerMiddleware(getMiddleware('composition')),
+        )
+
+    const renderers = layout.createRenderers({
+        services: {},
+    })
+
+    const wrapper = mount(
+        <DataProvider resources={resources} globalProps={{}}>
+            {renderers.renderCompositions(
+                layout.composition({
+                    type: 'test-with-data',
+                    contentAreas: {
+                        main: [
+                            layout.component({
+                                type: 'test-component',
+                                props: {},
+                            }),
+                        ],
+                    },
+                    props: {
+                        dataDefinitionArgs: {
+                            dataArg: 'Foo',
+                        },
+                    },
+                }),
+            )}
+        </DataProvider>,
+    )
+
+    await act(() => new Promise(resolve => setTimeout(resolve)))
+
+    let composition = wrapper.update().find(TestCompositionWithData)
+    expect(composition.text()).toBe('Length: 6#TestComponent#')
+    expect(composition.props()).toMatchObject({
+        dataProps: {
+            data: {
+                dataDefinitionArgs: { dataArg: 'Foo', multiplier: 2 },
+            },
+        },
+    })
+
+    act(() => {
+        updateMultiplier(3)
+    })
+    await act(() => new Promise(resolve => setTimeout(resolve)))
+
+    composition = wrapper.update().find(TestCompositionWithData)
+    expect(composition.text()).toBe('Length: 9#TestComponent#')
+    expect(composition.props()).toMatchObject({
+        dataProps: {
+            data: {
+                dataDefinitionArgs: { dataArg: 'Foo', multiplier: 3 },
+            },
+        },
+    })
+})
+
+const TestComponent: React.FC = () => <div>#TestComponent#</div>
+
 const TestComponentWithData: React.FC<{ length: number | undefined }> = ({ length }) => (
     <div>{length ? `Length: ${length}` : 'Loading'}</div>
 )
@@ -255,10 +421,25 @@ const TestComposition: React.FC<{ main: React.ReactElement<any> }> = props => (
     <div>{props.main}</div>
 )
 
+const TestCompositionWithData: React.FC<{
+    main: React.ReactElement<any>
+    length: number | undefined
+}> = ({ length, main }) => (
+    <div>
+        <div>{length ? `Length: ${length}` : 'Loading'}</div>
+        {main}
+    </div>
+)
+
+const { createRegisterableComposition, createRegisterableComponent } = getRegistrationCreators<{}>()
 const testCompositionRegistration = createRegisterableComposition<'main'>()(
     'test-composition',
     contentAreas => <TestComposition main={contentAreas.main} />,
 )
+
+const testComponentRegistration = createRegisterableComponent('test-component', () => (
+    <TestComponent />
+))
 
 const lengthCalculatorDataDefinition: DataDefinition<{ dataArg: string }, number, {}, {}> = {
     loadData: props =>
